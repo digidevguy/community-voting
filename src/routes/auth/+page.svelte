378c3ec44c68bb05d8e01eb055e162222dc33c3c
Matthew Littrell <matthew@mattlittrell.dev>
2025-11-29 21:04:17 -0600

    Added register option

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -66,7 +66,35 @@
 					<Card.Description>Create an account to start voting.</Card.Description>
 				</Card.Header>
 				<Card.Content class="grid gap-6">
-					<form action="?/register"></form>
+					<form action="?/register" method="POST" use:enhance={handleSubmit} class="grid gap-4">
+						<div class="grid gap-2">
+							<Label for="email">Email</Label>
+							<Input id="email" name="email" type="email" placeholder="iami@email.com" />
+						</div>
+						<div class="grid gap-2">
+							<Label for="username">Username</Label>
+							<Input id="username" name="username" type="text" placeholder="BossMan2025" />
+						</div>
+						<div class="grid gap-2">
+							<Label for="displayName">Display Name</Label>
+							<Input id="displayName" name="displayName" type="text" placeholder="Boss Man 2025" />
+						</div>
+						<div class="grid gap-2">
+							<Label for="password">Password</Label>
+							<Input id="password" name="password" type="password" placeholder="Your password" />
+						</div>
+						<div class="grid gap-2">
+							<Label for="confirmPassword">Password</Label>
+							<Input
+								id="confirmPassword"
+								name="confirmPassword"
+								type="password"
+								placeholder="Confirm your password"
+							/>
+						</div>
+						<Button type="submit">Submit</Button>
+						<p style="color: red">{form?.message ?? ''}</p>
+					</form>
 				</Card.Content>
 			</Card.Root>
 		</Tabs.Content>
