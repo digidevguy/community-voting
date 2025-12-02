1c152c6c960365584ece07f6931a319552e154af
Matthew <matthew@mattlittrell.dev>
2025-12-01 20:17:25 -0600

    Adding basic card styles

--- src/routes/voting/[votingSessionId]/+page.svelte
+++ src/routes/voting/[votingSessionId]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card/index';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import type { PageProps } from './$types';
 
@@ -43,42 +44,49 @@
 <Separator class="my-4" />
 <section>
 	<ul
-		class="grid grid-cols-[repeat(auto-fit,minmax(theme(spacing.48),1fr))] place-items-center gap-4"
+		class="grid grid-cols-[repeat(auto-fit,minmax(theme(spacing.64),1fr))] place-items-center gap-4"
 	>
 		{#each options as option}
-			<li class="flex max-w-xs flex-col">
-				{#if option.game?.image}
-					<img src={option.game.image} alt={option.game.title} />
-				{/if}
-				<p>{option.game?.title}</p>
-				<!-- Todo: add form w/ button -->
-				<form
-					action="?/vote"
-					method="POST"
-					use:enhance={() => {
-						submittingOptionId = option.id;
-						return async ({ update }) => {
-							await update();
-							submittingOptionId = undefined;
-						};
-					}}
-				>
-					<input type="hidden" name="votingSessionId" value={votingSessionDetails.id ?? ''} />
-					<input type="hidden" name="votingOptionId" value={option.id ?? ''} />
-					{#if form?.errors && Array.isArray(form.errors)}
-						<div class="text-red-600">
-							{#each form.errors as error}
-								<p>{error}</p>
-							{/each}
+			<li>
+				<Card.Root class=" overflow-hidden py-0 pb-6">
+					{#if option.game?.image}
+						<img src={option.game.image} alt={option.game.title} />
+					{/if}
+
+					<Card.Footer class="justify-between gap-2">
+						{option.voteCount === 1 ? '1 vote' : `${option.voteCount} votes`}
+						<div class="flex gap-2">
+							<Button variant="link">Learn More</Button>
+							<form
+								action="?/vote"
+								method="POST"
+								use:enhance={() => {
+									submittingOptionId = option.id;
+									return async ({ update }) => {
+										await update();
+										submittingOptionId = undefined;
+									};
+								}}
+							>
+								<input type="hidden" name="votingSessionId" value={votingSessionDetails.id ?? ''} />
+								<input type="hidden" name="votingOptionId" value={option.id ?? ''} />
+								{#if form?.errors && Array.isArray(form.errors)}
+									<div class="text-red-600">
+										{#each form.errors as error}
+											<p>{error}</p>
+										{/each}
+									</div>
+								{/if}
+								<Button type="submit" disabled={submittingOptionId === option.id}>
+									{submittingOptionId === option.id ? 'Voting...' : 'Vote'}
+								</Button>
+								{#if form?.success && submittingOptionId === option.id}
+									<p class="text-green-700">Vote successful: {form.voteId}</p>
+								{/if}
+							</form>
 						</div>
-					{/if}
-					<Button type="submit" disabled={submittingOptionId === option.id}>
-						{submittingOptionId === option.id ? 'Voting...' : 'Vote'}
-					</Button>
-					{#if form?.success && submittingOptionId === option.id}
-						<p class="text-green-700">Vote successful: {form.voteId}</p>
-					{/if}
-				</form>
+					</Card.Footer>
+				</Card.Root>
 			</li>
 		{/each}
 	</ul>
