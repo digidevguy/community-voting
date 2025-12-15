d4bd0f9c4b6217f0694943ce1665d3e2463066f9
Matthew <matthew@mattlittrell.dev>
2025-12-14 22:23:23 -0600

    Adding form date handling

--- src/routes/voting/create/[communityId]/+page.svelte
+++ src/routes/voting/create/[communityId]/+page.svelte
@@ -6,12 +6,13 @@
 	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import ChevronDown from '@lucide/svelte/icons/chevron-down';
+	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
 	import { getLocalTimeZone } from '@internationalized/date';
-	import type { CalendarDate } from '@internationalized/date';
+	import { CalendarDate } from '@internationalized/date';
 	import * as Select from '$lib/components/ui/select/index.js';
 	import * as Card from '$lib/components/ui/card';
 	import * as Popover from '$lib/components/ui/popover/index.js';
+	import Calendar from '$lib/components/ui/calendar/calendar.svelte';
 
 	let { data, form }: PageProps = $props();
 
@@ -19,11 +20,11 @@
 	let searchQuery = $state('');
 	let searchResults = $state<Array<{ id: string; title: string; type: string }>>([]);
 	let selectedGames = $state<Array<{ id: string; title: string }>>([]);
+	let startDate = $state<CalendarDate | undefined>();
+	let gameDayDate = $state<CalendarDate | undefined>();
 
-	const id = $props.id();
-
-	let open = $state(false);
-	let value = $state<CalendarDate | undefined>();
+	let startDateOpen = $state(false);
+	let gameDayOpen = $state(false);
 
 	$effect(() => {
 		if (form?.games) {
@@ -72,8 +73,86 @@
 			</Select.Root>
 		</div>
 		<!-- Obtain voting start date here -->
+		<div class="flex justify-center gap-6">
+			<div class="flex flex-col gap-2">
+				<h2 class="text-lg font-semibold">Voting start date</h2>
+				<div class="flex gap-4">
+					<div class="flex flex-col gap-3">
+						<Label for="startDate-date" class="px-1">Date</Label>
+						<Popover.Root bind:open={startDateOpen}>
+							<Popover.Trigger id="startDate">
+								{#snippet child({ props })}
+									<Button {...props} class="w-32 justify-between font-normal" variant="outline">
+										{startDate
+											? startDate.toDate(getLocalTimeZone()).toLocaleDateString()
+											: 'Select date'}
+										<ChevronDownIcon />
+									</Button>
+								{/snippet}
+							</Popover.Trigger>
+							<Popover.Content class="w-auto overflow-hidden p-0" align="start">
+								<Calendar
+									type="single"
+									bind:value={startDate}
+									onValueChange={() => (startDateOpen = false)}
+									captionLayout="dropdown"
+								/>
+							</Popover.Content>
+						</Popover.Root>
+					</div>
+					<div class="flex flex-col gap-3">
+						<Label class="px-1" id="startDateTime">Time</Label>
+						<Input
+							class="appearance-none bg-background [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+							step="1"
+							value="10:30:00"
+							type="time"
+							id="startDateTime"
+						/>
+					</div>
+				</div>
+			</div>
 
-		<!-- Game day date here -->
+			<!-- Game day date here -->
+			<div class="flex flex-col gap-2">
+				<h2 class="text-lg font-semibold">Game day date</h2>
+				<div class="flex gap-4">
+					<div class="flex flex-col gap-3">
+						<Label for="gameDaydate" class="px-1">Date</Label>
+						<Popover.Root bind:open={gameDayOpen}>
+							<Popover.Trigger id="startDate">
+								{#snippet child({ props })}
+									<Button {...props} class="w-32 justify-between font-normal" variant="outline">
+										{gameDayDate
+											? gameDayDate.toDate(getLocalTimeZone()).toLocaleDateString()
+											: 'Select date'}
+										<ChevronDownIcon />
+									</Button>
+								{/snippet}
+							</Popover.Trigger>
+							<Popover.Content class="w-auto overflow-hidden p-0" align="start">
+								<Calendar
+									type="single"
+									bind:value={gameDayDate}
+									onValueChange={() => (gameDayOpen = false)}
+									captionLayout="dropdown"
+								/>
+							</Popover.Content>
+						</Popover.Root>
+					</div>
+					<div class="flex flex-col gap-3">
+						<Label class="px-1" id="gameDayDateTime">Time</Label>
+						<Input
+							class="appearance-none bg-background [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+							step="1"
+							value="10:30:00"
+							type="time"
+							id="startDateTime"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		{#each selectedGames as game}
 			<input type="hidden" name="gameIds" value={game.id} />
