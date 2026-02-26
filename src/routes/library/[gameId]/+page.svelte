3f3c4197404dbd99bfe300500c3f596445851dce
Matthew Littrell <matthew@mattlittrell.dev>
2026-02-25 22:30:40 -0600

    Adding basic game info elements.

--- src/routes/library/[gameId]/+page.svelte
+++ src/routes/library/[gameId]/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
 	import { ExternalLink } from '@lucide/svelte';
 	import type { PageData } from './$types';
 	import { format } from 'date-fns';
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
 {#if data.game}
-	<img src={data.game.image} alt={data.game.title} />
-	<h1>{data.game.title}</h1>
+	<img src={data.game.image} alt={data.game.title} class="mx-auto mb-4 rounded-sm" />
+	<h1 class="text-2xl font-semibold">{data.game.title}</h1>
 	<p>{data.game.description}</p>
 	<div>
-		<p>{data.game.developer}</p>
-		<p>{data.game.publisher}</p>
+		<p>Developer: {data.game.developer}</p>
+		<p>Pulisher: {data.game.publisher}</p>
 		{#if data.game.releaseDate}
-			<p>{format(data.game.releaseDate, 'MMM Mo, yyyy')}</p>
+			<p>Release date: {format(data.game.releaseDate, 'MMM Mo, yyyy')}</p>
 		{/if}
 		{#if data.game.genres}
-			<ul>
+			<ul class="flex gap-2 p-2">
 				{#each data.game.genres as genre}
-					<!-- Todo: convert to badges -->
-					<li>{genre}</li>
+					<li><Badge>{genre}</Badge></li>
 				{/each}
 			</ul>
 		{/if}
