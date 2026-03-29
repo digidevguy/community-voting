02784d536eaf4c3bebe5f0f025abfbfbefa52c3e
Matthew Littrell <matthew@mattlittrell.dev>
2026-03-28 23:53:55 -0500

    Fix: Adding join existing flow

--- src/routes/community/[communityId]/join/[inviteId]/+page.svelte
+++ src/routes/community/[communityId]/join/[inviteId]/+page.svelte
@@ -19,58 +19,85 @@
 	<Card.Root>
 		<Card.Header>
 			<Card.Title>Welcome to the {data.community.title} Community!</Card.Title>
-			<Card.Description>Let's make an account!</Card.Description>
+			{#if data.isAuthenticated}
+				<Card.Description>Let's make an account!</Card.Description>
+			{/if}
 		</Card.Header>
 		<Card.Content>
-			<form
-				action="?/join"
-				method="post"
-				use:enhance={() => {
-					submitting = true;
-					return async ({ result, update }) => {
-						if (result.type === 'success') {
-							toast.success('Account created successfully!');
-							await applyAction({
-								type: 'redirect',
-								status: 303,
-								location: `/community/${result.data?.communityId}`
-							});
-							return;
-						}
-						await update();
-						submitting = false;
-					};
-				}}
-				class="space-y-2"
-			>
-				<div class="space-y-2">
-					<Label for="username">Username</Label>
-					<Input id="username" name="username" bind:value={username} />
-				</div>
-				<input type="hidden" id="displayName" name="displayName" bind:value={displayName} />
-				<div class="space-y-2">
-					<Label for="email">Email</Label>
-					<Input id="email" name="email" />
-				</div>
-				<div class="space-y-2">
-					<Label for="password">Password</Label>
-					<Input id="password" name="password" type="password" />
-				</div>
-				<div class="space-y-2">
-					<Label for="confirmPassword">Confirm Password</Label>
-					<Input id="confirmPassword" name="confirmPassword" type="password" />
-				</div>
-				<Button type="submit" class="mt-2 w-full">
-					{#if submitting}
-						<LoaderCircle class="animate-spin" />
-					{:else}
-						Create
+			{#if data.isAuthenticated}
+				<form
+					method="POST"
+					action="?/joinExisting"
+					use:enhance={() => {
+						submitting = true;
+						return async ({ result, update }) => {
+							if (result.type === 'success') {
+								toast.success('Account linked to community!');
+								await applyAction({
+									type: 'redirect',
+									status: 303,
+									location: `/community/${data.community.id}`
+								});
+								return;
+							}
+							await update();
+							submitting = false;
+						};
+					}}
+				>
+					<Button type="submit">Join</Button>
+				</form>
+			{:else}
+				<form
+					action="?/join"
+					method="post"
+					use:enhance={() => {
+						submitting = true;
+						return async ({ result, update }) => {
+							if (result.type === 'success') {
+								toast.success('Account created successfully!');
+								await applyAction({
+									type: 'redirect',
+									status: 303,
+									location: `/community/${result.data?.communityId}`
+								});
+								return;
+							}
+							await update();
+							submitting = false;
+						};
+					}}
+					class="space-y-2"
+				>
+					<div class="space-y-2">
+						<Label for="username">Username</Label>
+						<Input id="username" name="username" bind:value={username} />
+					</div>
+					<input type="hidden" id="displayName" name="displayName" bind:value={displayName} />
+					<div class="space-y-2">
+						<Label for="email">Email</Label>
+						<Input id="email" name="email" />
+					</div>
+					<div class="space-y-2">
+						<Label for="password">Password</Label>
+						<Input id="password" name="password" type="password" />
+					</div>
+					<div class="space-y-2">
+						<Label for="confirmPassword">Confirm Password</Label>
+						<Input id="confirmPassword" name="confirmPassword" type="password" />
+					</div>
+					<Button type="submit" class="mt-2 w-full">
+						{#if submitting}
+							<LoaderCircle class="animate-spin" />
+						{:else}
+							Create
+						{/if}
+					</Button>
+					{#if form?.message}
+						<p class="text-red-600">{form?.message}</p>
 					{/if}
-				</Button>
-				{#if form?.message}
-					<p class="text-red-600">{form?.message}</p>
-				{/if}
-			</form>
+				</form>
+			{/if}
 		</Card.Content>
 	</Card.Root>
 </div>
