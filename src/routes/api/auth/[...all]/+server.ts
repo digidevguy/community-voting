7e9903831a4d133931e7a72c78bae40eab4e6d2d
Matthew Littrell <matthew@mattlittrell.dev>
2026-04-01 12:24:56 -0500

    Add: Created catch-all auth route and auth client

---
+++ src/routes/api/auth/[...all]/+server.ts
@@ -0,0 +1,6 @@
+import { auth } from '$lib/server/auth';
+import { toSvelteKitHandler } from 'better-auth/svelte-kit';
+
+const handler = toSvelteKitHandler(auth);
+
+export { handler as GET, handler as POST };
